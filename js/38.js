d8023ff512baeec444c9a089f3aea426f5d73a85
kde1989 <k.d.e.1989@gmail.com>
2023-03-07 18:47:30 +0300

    Task added - Word Blanks

---
+++ js/38.js
@@ -0,0 +1,8 @@
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks = "My " + myNoun + " is very " + myAdjective + "," + " but " + myVerb + " very " + myAdverb + "!";
+console.log(wordBlanks)
